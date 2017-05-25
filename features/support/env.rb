8e6e9153605f06a23c482e27fb95337134495690
Lucas Melo <lucasmelo@MacBook-Pro-de-LP1396.local>
2017-05-24 22:30:18 -0300

    Atualização do Projeto

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,17 +8,12 @@ require 'site_prism'
 require 'yaml'
 require 'httparty'
 
-# loads factories
-# require_relative "../../fixtures/factories/simulation_api.rb"
-
 include Capybara::DSL
 RSpec.configure do |config|
   config.include Capybara::DSL, :type => :feature
 end
 
-# Environment definitions
 ENVIRONMENT = YAML.load_file('./config/environment.yml')[ENV['ENV']]
-# DEFAULT_DATA = YAML.load_file('./fixtures/data_files/datafile.yml')[ENV['ENV']]
 
 Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
